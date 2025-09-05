91480bd2baffdcf24ec65693f6ba6a7e054514b5
Yago Inacio <yagofaran@gmail.com>
2025-09-05 17:36:20 -0300

    auto redirect from base path to playground

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { BooksModule } from './books/books.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
@@ -23,6 +22,5 @@ import { BooksModule } from './books/books.module';
     BooksModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
